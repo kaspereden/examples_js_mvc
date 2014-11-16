e7aad3ac058b21f80e4b68a1c47e1cd75b44c4a7
Kasper Reijnders <kasperreijnders@gmail.com>
2014-11-16 22:02:11 +0100

    Added the simple example

---
+++ js/1.js
@@ -0,0 +1,30 @@
+(function () {
+	'use strict';
+	var example1 = document.getElementById('example1');
+	var model = {
+	    value: 'nothing'
+	};
+
+	function updateModel() {
+	    var containers = example1.getElementsByClassName('mine');
+	    var cLength = containers.length;
+	    for (var i = 0; i< cLength; i = i+1) {
+	        if (containers[i].value !== undefined) {
+	            containers[i].value = model.value;
+	        }
+	        containers[i].textContent = model.value;
+	    }
+
+	}
+
+	updateModel();
+
+	var inputs = example1.getElementsByClassName('mineInput');
+	var cLength = inputs.length;
+	for (var i = 0; i < cLength; i = i+1) {
+	    inputs[i].onchange = function() {
+	        model.value=this.value;
+	        updateModel();
+	    };
+	}
+}());

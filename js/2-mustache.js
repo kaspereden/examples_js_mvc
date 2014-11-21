837b2b0f016ac07576464cdf8a80c7f3a9920a76
Kasper Reijnders <kasperreijnders@gmail.com>
2014-11-21 10:46:36 +0100

    Moustache example

--- js/2-mustache.js
+++ js/2-mustache.js
@@ -1,4 +1,22 @@
 (function () {
 	'use strict';
-	
+	var example2 = document.getElementById('example2');
+
+	var model = { value: '#worldofincentro' };
+	var view = '<p><a href="#">{{value}}</a> text</p>' +
+		'<p>{{value}}</p>' +
+		'<div class="container">' +
+		'<input class="mineInput" value="{{value}}"/>' +
+		'</div>';
+
+	example2.getElementsByClassName('mine-container')[0].innerHTML = Mustache.render(view, model);
+
+	var inputs = example2.getElementsByClassName('mineInput');
+	var cLength = inputs.length;
+	for (var i = 0; i < cLength; i = i+1) {
+		inputs[i].onchange = function() {
+			model.value=this.value;
+			example2.getElementsByClassName('mine-container')[0].innerHTML = Mustache.render(view, model);
+		};
+	}
 } ());
